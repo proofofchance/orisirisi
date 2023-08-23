c4c751e5421eaa49856cee8fd608636e1657722e
Joshua Oladele <jurshsmith@gmail.com>
2023-08-23 01:27:34 +0100

    Part of format changes

--- libs/coinflip-contracts/src/Coinflip/Game.sol
+++ libs/coinflip-contracts/src/Coinflip/Game.sol
@@ -4,36 +4,33 @@ pragma solidity ^0.8.18;
 import {Coin} from './Coin.sol';
 
 library Game {
-  type Wager is uint;
-  type ID is uint;
+    type Wager is uint;
+    type ID is uint;
 
-  type PlayID is uint16;
-  type Player is address;
+    type PlayID is uint16;
+    type Player is address;
 
-  struct Play {
-    Player player;
-    Coin.Side coinSide;
-    bytes32 proofOfChance;
-    bytes32 playHash;
-  }
-
-  error MaxedOutPlaysError(ID gameID);
-  error allMatchingPlaysError(ID gameID);
-
-  enum Status {
-    Ongoing,
-    Expired,
-    WinnersUnresolved,
-    Concluded
-  }
-
-  function getEntropy(bytes32 proofOfChance) internal pure returns (uint16) {
-    uint16 sum = 0;
-
-    for (uint8 i = 0; i < 32; i++) {
-      sum += uint8(proofOfChance[i]);
+    struct Play {
+        Player player;
+        Coin.Side coinSide;
+        bytes32 proofOfChance;
+        bytes32 playHash;
     }
 
-    return sum;
-  }
+    enum Status {
+        Ongoing,
+        Expired,
+        WinnersUnresolved,
+        Concluded
+    }
+
+    function getEntropy(bytes32 proofOfChance) internal pure returns (uint16) {
+        uint16 sum = 0;
+
+        for (uint8 i = 0; i < 32; i++) {
+            sum += uint8(proofOfChance[i]);
+        }
+
+        return sum;
+    }
 }
