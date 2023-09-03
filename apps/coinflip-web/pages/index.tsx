42152d9050a186cf677e281be0fa93b7223b7f0b
Joshua Oladele <jurshsmith@gmail.com>
2023-09-03 13:08:07 +0100

    Only show GameSection when connected

--- apps/coinflip-web/pages/index.tsx
+++ apps/coinflip-web/pages/index.tsx
@@ -4,14 +4,17 @@ import {
   CreateGameSection,
   NavigationBar,
 } from '@orisirisi/coinflip-web-ui';
+import { useIsWeb3Connected } from '@orisirisi/orisirisi-web3-ui';
 
 export function Index() {
+  const isWeb3Connected = useIsWeb3Connected();
+
   return (
     <>
       <ConnectWalletOptionsModal />
       <Background className="px-20">
         <NavigationBar className="py-4" />
-        <CreateGameSection />
+        {isWeb3Connected && <CreateGameSection />}
       </Background>
     </>
   );
