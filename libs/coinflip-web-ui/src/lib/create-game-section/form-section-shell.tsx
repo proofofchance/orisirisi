20051b65c503b3d9c93602ba24379b825461fc45
Joshua Oladele <jurshsmith@gmail.com>
2023-09-06 16:00:25 +0100

    Allow tips in FormShell

--- libs/coinflip-web-ui/src/lib/create-game-section/form-section-shell.tsx
+++ libs/coinflip-web-ui/src/lib/create-game-section/form-section-shell.tsx
@@ -1,15 +1,21 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 
 interface Props extends PropsWithChildren {
   title: string;
+  tip?: ReactElement | string;
 }
 
-export function FormSectionShell({ title, children }: Props) {
+export function FormSectionShell({ title, tip, children }: Props) {
   return (
     <section className="text-center text-white">
-      <h2 className="mt-10 font-bold text-[28px] tracking-wide">{title}</h2>
+      <h2 className="mt-8 font-bold text-[28px] tracking-wide">{title}</h2>
+      {tip && (
+        <p className="mt-2 tracking-tight text-sm max-w-[600px] m-auto">
+          {tip}
+        </p>
+      )}
       <div className="flex justify-center">
-        <div className="w-[600px">{children}</div>
+        <div>{children}</div>
       </div>
     </section>
   );
