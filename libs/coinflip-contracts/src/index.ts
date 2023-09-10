6a1f170eb1299a27317c5e293f33c7d261cb7643
Joshua Oladele <jurshsmith@gmail.com>
2023-09-10 01:02:44 +0100

    Allow building Coinflip Contract fromSigner

--- libs/coinflip-contracts/src/index.ts
+++ libs/coinflip-contracts/src/index.ts
@@ -1,13 +1,17 @@
 // Coinflip Smart Contracts
 // This will house reasonable scripts for running Coinflip contracts
 
+import { Signer } from 'ethers';
 import { ChainID } from '@orisirisi/orisirisi-web3-chains';
-
+import { Coinflip__factory } from '../typechain-types';
 import coinflipLocalhostDeployment from '../deployments/localhost/Coinflip.json';
 
 export class CoinflipContract {
-  static getAddress(chaindID: ChainID) {
-    switch (chaindID) {
+  static fromSigner(signer: Signer, chainId: ChainID) {
+    return Coinflip__factory.connect(this.getAddress(chainId), signer);
+  }
+  static getAddress(chainId: ChainID) {
+    switch (chainId) {
       case ChainID.Localhost:
         return coinflipLocalhostDeployment.address;
       default:
