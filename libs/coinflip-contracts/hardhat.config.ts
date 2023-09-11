388f65174ca5ea7869fdc2e83e2c24f02fb8f310
Joshua Oladele <jurshsmith@gmail.com>
2023-09-11 12:36:45 +0100

    Improve TSConfig with path aliasing

--- libs/coinflip-contracts/hardhat.config.ts
+++ libs/coinflip-contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-deploy';
+import 'tsconfig-paths/register';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
