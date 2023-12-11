4773fc1a4d669460666c0cec927b849f2e1ed56c
Joshua Oladele <jurshsmith@gmail.com>
2023-12-11 23:10:30 +0100

    Add inner context test template

---
+++ libs/coinflip-contracts/tests/Coinflip/GamePlays.test.ts
@@ -0,0 +1,10 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+
+describe('GamePlays', () => {
+  describe('createGamePlay', () => {
+    it('creates game play', async () => {
+      console.log('Runs create game play test');
+    });
+  });
+});
