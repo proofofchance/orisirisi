634382f79727a19dfc0da621a26d7a605976b8b5
Joshua Oladele <jurshsmith@gmail.com>
2024-01-02 18:58:30 +0100

    Introduce useOnClickOutside hook

--- libs/orisirisi-web-ui/src/index.ts
+++ libs/orisirisi-web-ui/src/index.ts
@@ -3,3 +3,4 @@ export { useIsClient } from './lib/use-is-client';
 export * from './lib/types';
 export * from './lib/input-fields';
 export { Provider } from './lib/provider';
+export { useOnClickOutside } from './lib/use-on-click-outside';
---
+++ libs/orisirisi-web-ui/src/lib/use-on-click-outside.ts
@@ -0,0 +1,22 @@
+import { useEffect, RefObject } from 'react';
+
+export const useOnClickOutside = <T extends HTMLElement>(
+  ref: RefObject<T>,
+  handler: () => void
+): void => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler();
+      }
+    };
+
+    // Attach the event listener
+    document.addEventListener('click', handleClickOutside);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [ref, handler]);
+};
