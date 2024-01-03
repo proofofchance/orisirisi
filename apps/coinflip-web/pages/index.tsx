93de150125cdfade7c7376c840b1114f5f46f3d8
Joshua Oladele <jurshsmith@gmail.com>
2024-01-03 08:11:36 +0100

    Redirect to /games page from /

--- apps/coinflip-web/pages/index.tsx
+++ apps/coinflip-web/pages/index.tsx
@@ -1,5 +1,13 @@
+import { useIsClient } from '@orisirisi/orisirisi-web-ui';
+import { useRouter } from 'next/router';
+
 export function Index() {
-  return <></>;
+  const isClient = useIsClient();
+  const { replace } = useRouter();
+
+  isClient && replace('/games');
+
+  return null;
 }
 
 export default Index;
