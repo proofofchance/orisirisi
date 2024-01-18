709ad821532218c190e674240b92502f8bfd7714
Joshua Oladele <jurshsmith@gmail.com>
2024-01-18 20:02:49 +0100

    Update copy

--- libs/coinflip-web-ui/src/lib/game-page/game-activities-view.tsx
+++ libs/coinflip-web-ui/src/lib/game-page/game-activities-view.tsx
@@ -56,7 +56,7 @@ export function GameActivitiesView({
           <Coin side={null} flipInfinitely className="self-center" />
 
           <p className="text-sm self-end mt-6 mb-2">
-            The outcome will be shown in any moment from now...
+            Refresh to see the outcome in a minute...
           </p>
         </div>
       );
