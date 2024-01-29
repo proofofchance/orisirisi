cff31f7fc6cbb4a5d5f73b3915c7ed6c3f5da5ad
Joshua Oladele <jurshsmith@gmail.com>
2024-01-29 14:55:50 +0100

    Update solidity version

--- libs/coinflip-contracts/src/Ownable.sol
+++ libs/coinflip-contracts/src/Ownable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.24;
 
 abstract contract Ownable {
     address private _owner;
--- libs/coinflip-contracts/src/Payments.sol
+++ libs/coinflip-contracts/src/Payments.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.24;
 
 library Payments {
     function pay(address to, uint256 amount) internal {
