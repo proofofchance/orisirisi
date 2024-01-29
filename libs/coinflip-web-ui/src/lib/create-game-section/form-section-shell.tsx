3fd5be2bde940f389a3b894e5b14223656b76904
Joshua Oladele <jurshsmith@gmail.com>
2024-01-29 13:29:28 +0100

    Remove tip from form section shell

--- libs/coinflip-web-ui/src/lib/create-game-section/form-section-shell.tsx
+++ libs/coinflip-web-ui/src/lib/create-game-section/form-section-shell.tsx
@@ -1,19 +1,13 @@
-import { PropsWithChildren, ReactElement } from 'react';
+import { PropsWithChildren } from 'react';
 
 interface Props extends PropsWithChildren {
   title: string;
-  tip?: ReactElement | string;
 }
 
-export function FormSectionShell({ title, tip, children }: Props) {
+export function FormSectionShell({ title, children }: Props) {
   return (
     <section className="text-center text-white">
       <h2 className="mt-8 font-bold text-[28px] tracking-wide">{title}</h2>
-      {tip && (
-        <p className="mt-2 tracking-tight text-sm max-w-[600px] m-auto">
-          {tip}
-        </p>
-      )}
       <div className="flex justify-center">
         <div>{children}</div>
       </div>
