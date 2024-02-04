b8d1346bdc843d2b6a95fdbd25b8277c27748732
Joshua Oladele <jurshsmith@gmail.com>
2024-02-04 22:15:55 +0100

    Fix getRandomInteger implementation

--- libs/orisirisi-data-utils/src/lib/orisirisi-data-utils.ts
+++ libs/orisirisi-data-utils/src/lib/orisirisi-data-utils.ts
@@ -16,7 +16,7 @@ export const parseInteger = (value: string | number) =>
   value ? parseInt(`${value}`, 10) : null;
 
 export function getRandomInteger(max: number, minimum = 0) {
-  return minimum + Math.round(Math.random() * max);
+  return minimum + Math.floor(Math.random() * (max - minimum + 1));
 }
 
 export function getDivisionAndRemainder(
