7d6bfbc3571412000328d620a89f8189f52e8671
Joshua Oladele <jurshsmith@gmail.com>
2024-02-06 22:56:26 +0100

    Deploy on sepolia testnet

--- libs/coinflip-contracts/hardhat.config.ts
+++ libs/coinflip-contracts/hardhat.config.ts
@@ -3,8 +3,17 @@ import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-deploy';
 import 'tsconfig-paths/register';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
+  networks: {
+    sepolia: {
+      url: process.env['SEPOLIA_JSON_RPC_URL'],
+      accounts: [process.env['SEPOLIA_PRIVATE_KEY']!],
+    },
+  },
   paths: {
     sources: './src',
     tests: './tests',
