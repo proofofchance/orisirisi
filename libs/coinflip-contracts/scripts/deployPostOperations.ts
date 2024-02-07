adab6755bbc182fb1e1979befbd3780a69db7a70
Joshua Oladele <jurshsmith@gmail.com>
2024-02-07 09:46:46 +0100

    Add loggin to deployment post operations

--- libs/coinflip-contracts/scripts/deployPostOperations.ts
+++ libs/coinflip-contracts/scripts/deployPostOperations.ts
@@ -3,12 +3,14 @@ import { Chain } from '@orisirisi/orisirisi-web3-chains';
 import { CoinflipContract, WalletsContract } from '../src';
 
 async function main() {
+  console.log('Adding Coinflip app to Wallets...');
   const chain = Chain.fromChainID(network.config.chainId);
   const wallets = WalletsContract.fromSignerAndChain(
     (await ethers.getSigners())[0],
     chain.id
   );
   await wallets.addApp(CoinflipContract.getAddress(chain.id));
+  console.log('Adding Coinflip app added successfully');
 }
 
 main().catch((error) => {
