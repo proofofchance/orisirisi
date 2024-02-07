de402d87d856cf0aa7938777ac543eb121809602
Joshua Oladele <jurshsmith@gmail.com>
2024-02-07 22:54:48 +0100

    Avoid populating empty networks when not set in env

--- libs/coinflip-contracts/hardhat.config.ts
+++ libs/coinflip-contracts/hardhat.config.ts
@@ -1,19 +1,37 @@
 import { HardhatUserConfig } from 'hardhat/config';
+import { HttpNetworkUserConfig, NetworksUserConfig } from 'hardhat/types';
 import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-deploy';
 import 'tsconfig-paths/register';
 
 import dotenv from 'dotenv';
+
 dotenv.config();
+
+const networks: NetworksUserConfig = {
+  sepolia: {
+    url: process.env['SEPOLIA_JSON_RPC_URL'],
+    accounts: [process.env['SEPOLIA_PRIVATE_KEY']!],
+  },
+};
+const nonEmptyNetworks = Object.keys(networks).reduce(
+  (nonEmptyNetworks, network) => {
+    const config = networks[network] as HttpNetworkUserConfig;
+    const hasUrl = config.url;
+    if (hasUrl) {
+      nonEmptyNetworks[network] = config;
+    }
+
+    return nonEmptyNetworks;
+  },
+  {} as NetworksUserConfig
+);
+
+console.log({ nonEmptyNetworks });
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
-  networks: {
-    sepolia: {
-      url: process.env['SEPOLIA_JSON_RPC_URL'],
-      accounts: [process.env['SEPOLIA_PRIVATE_KEY']!],
-    },
-  },
+  networks: nonEmptyNetworks,
   paths: {
     sources: './src',
     tests: './tests',
