e546ee993e2e80f511c5003c8b64a1967f389a12
Joshua Oladele <jurshsmith@gmail.com>
2024-03-10 13:15:34 +0100

    Only run effect on mount

--- libs/orisirisi-web-ui/src/lib/use-page-visibility.ts
+++ libs/orisirisi-web-ui/src/lib/use-page-visibility.ts
@@ -20,5 +20,6 @@ export function usePageVisibility(
     return () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-  }, [ignoreOnload, onPageVisible]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 }
