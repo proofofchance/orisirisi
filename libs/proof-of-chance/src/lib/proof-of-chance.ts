6987e48dcfeb30e23fecb15bd1c2ff52a16b3494
Joshua Oladele <jurshsmith@gmail.com>
2024-03-17 09:16:21 +0100

    Fix fromFileContent

--- libs/proof-of-chance/src/lib/proof-of-chance.ts
+++ libs/proof-of-chance/src/lib/proof-of-chance.ts
@@ -96,7 +96,7 @@ export class ProofOfChance {
   static fromFileContent(fileContent: string) {
     const chance = this.getFromFileContent(
       fileContent,
-      'Your lucky charm in hexadecimal:'
+      'Your chance a.k.a your Lucky Charm:'
     );
     const salt = this.getFromFileContent(
       fileContent,
